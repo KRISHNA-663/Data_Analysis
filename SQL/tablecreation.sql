bba75a36d81766da080dfe5355b2465681784c3d
KRISHNA-663 <krish7cbe@gmail.com>
2024-01-24 09:17:46 +0530

    DA

---
+++ SQL/tablecreation.sql
@@ -0,0 +1,4 @@
+create table groceries(id Integer PRIMARY KEY,Name Text,Quantity Integer);
+Insert into groceries values(1,"Flour",2);
+Insert into groceries values(2,"Maida",3);
+SELECT * from groceries;
