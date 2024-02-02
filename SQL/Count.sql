d1a2aa9617bbb7c6dd90cd7aea73077048c0f3df
KRISHNA-663 <krish7cbe@gmail.com>
2024-02-02 15:58:22 +0530

    SQL

---
+++ SQL/Count.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(DISTINCT bike_id) AS no_of_bikes
+FROM `bigquery-public-data.london_bicycles.cycle_hire` 
+WHERE end_station_name ='Moor Street, Soho';
+
+--BigQuery
+--If AS is not used(name of the new column is not mentioned), then SQL uses default names like f0,f1.
