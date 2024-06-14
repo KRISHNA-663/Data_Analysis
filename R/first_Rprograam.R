108837b540727aa3f0b714bc0607c9d43dbddc84
KRISHNA-663 <krish7cbe@gmail.com>
2024-06-14 18:19:29 +0530

    DA

---
+++ R/first_Rprograam.R
@@ -0,0 +1,8 @@
+#This is my first R program
+val<-5
+print(val)
+#Vector
+res<-c(30,10,4,19) #c denotes the combination function
+print(res)
+
+#Pipe is a way to chain multiple operation together and is represented by %>%
