2ba73d8ad08c0cb2d0cd6b7215deb4e36dd41814
KRISHNA-663 <krish7cbe@gmail.com>
2024-06-25 09:28:32 +0530

    R

--- R/ggplot2_1.R
+++ R/ggplot2_1.R
@@ -3,10 +3,13 @@ library(tidyverse)
 library(ggplot2)
 library(palmerpenguins)
 
+View(penguins)
+ggplot(data = penguins)+geom_point(mapping = aes(x=flipper_length_mm,y=body_mass_g))+facet_wrap(~island)
 
-ggplot(data = penguins)+geom_point(mapping = aes(x=flipper_length_mm,y=body_mass_g))+facet_wrap(~species)
+ggsave("Testing_save_function.png")
 
 ggplot(data = penguins) #creates empty plot
 
 ?geom_point
 ?facet_wrap
+
