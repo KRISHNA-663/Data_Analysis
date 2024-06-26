d9081609f13a8868a46a7896533d9919f5666828
KRISHNA-663 <krish7cbe@gmail.com>
2024-06-26 07:10:46 +0530

    R

---
+++ R/ggplot_basics.R
@@ -0,0 +1,9 @@
+#Chart title
+#To add a title to the chart, use a label function: title = Average product rating.
+
+# Blue and yellow bars
+# To highlight underperforming products, use an aesthetics function: col = ifelse (x<2, 'blue', 'yellow').
+
+# Trend line
+# To create a trend line, use a geom function: geom_smooth ().
+
