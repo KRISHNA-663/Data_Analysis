c3d2d84a186d3b97c86a80333a0f9d64ba81b3f5
KRISHNA-663 <krish7cbe@gmail.com>
2024-07-14 17:23:52 +0530

    changes

--- PYTHON/end_arg.py
+++
@@ -1,2 +0,0 @@
-print("Hello!",end="") #end argument is used for specifying for happens at the end of the line
-print("Krishna")
--- PYTHON/ternary_op.py
+++
@@ -1,3 +0,0 @@
-age = int(input())
-
-print("Adult"if(age>18) else "Child")
